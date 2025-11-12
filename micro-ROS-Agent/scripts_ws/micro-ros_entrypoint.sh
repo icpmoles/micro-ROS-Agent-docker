d10948690dce10467679728d614fcb7758e828bf
Iacopo Moles <iacopo@icpmol.es>
2025-11-12 14:57:44 +0100

    add override for FASTRTPS_DEFAULT_PROFILES_FILE path for use at run

--- micro-ROS-Agent/scripts_ws/micro-ros_entrypoint.sh
+++ micro-ROS-Agent/scripts_ws/micro-ros_entrypoint.sh
@@ -1,13 +1,18 @@
 . "/opt/ros/$ROS_DISTRO/setup.sh"
 . "/uros_ws/install/local_setup.sh"
 
-if [ "$MICROROS_DISABLE_SHM" = "1" ] ; then
-    if [ "$ROS_LOCALHOST_ONLY" = "1" ] ; then
-        export FASTRTPS_DEFAULT_PROFILES_FILE=$COLCON_WS_PATH/dds_config/disable_fastdds_shm_localhost_only.xml
-    else
-        export FASTRTPS_DEFAULT_PROFILES_FILE=$COLCON_WS_PATH/dds_config/disable_fastdds_shm.xml
+# if variable is not set, uses built in
+if [ -z ${FASTRTPS_DEFAULT_PROFILES_FILE+x} ]; then
+    if [ "$MICROROS_DISABLE_SHM" = "1" ] ; then
+        if [ "$ROS_LOCALHOST_ONLY" = "1" ] ; then
+            export FASTRTPS_DEFAULT_PROFILES_FILE=$COLCON_WS_PATH/dds_config/disable_fastdds_shm_localhost_only.xml
+        else
+            export FASTRTPS_DEFAULT_PROFILES_FILE=$COLCON_WS_PATH/dds_config/disable_fastdds_shm.xml
+        fi
     fi
 fi
+    
+
 echo $(env)
 echo "$@"
 ls -alt /dev | grep "tty"
